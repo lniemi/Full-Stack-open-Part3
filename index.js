2462ddcf98cbb724071a4a3bdfe709529ac1e48a
lniemi <90403204+lniemi@users.noreply.github.com>
2023-04-10 01:48:34 +0300

    Step 11 (3.11)

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
@@ -33,8 +35,9 @@ let persons = [
     }
 ]
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.json())
+
+
+
 
 
 app.get('/info', (request, response) => {
