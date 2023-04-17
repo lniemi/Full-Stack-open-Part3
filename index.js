36756b5618ce77a6912f1bc1d1efe55a3d540903
lniemi <90403204+lniemi@users.noreply.github.com>
2023-04-17 15:27:15 +0300

    Step 4 (3.16) Error Handling

--- index.js
+++ index.js
@@ -24,67 +24,84 @@ app.get('/info', (request, response) => {
     })
   })
 
-  app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person)
-        } else {
-          response.status(404).end()
-        }
-      })
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
-  app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({
-        error: 'name missing'
-      })
-    }
-  
-    if (!body.number) {
-      return response.status(400).json({
-        error: 'number missing'
-      })
-    }
-  
-    Person.findOne({ name: body.name })
-      .then(existingPerson => {
-        if (existingPerson) {
-          return response.status(400).json({
-            error: 'name must be unique'
-          })
-        }
-  
-        const person = new Person({
-          name: body.name,
-          number: body.number
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  Person.findOne({ name: body.name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return response.status(400).json({
+          error: 'name must be unique'
         })
-  
-        person.save()
-          .then(savedPerson => {
-            response.json(savedPerson)
-          })
+      }
+
+      const person = new Person({
+        name: body.name,
+        number: body.number
       })
-  })
-  
+
+      person.save()
+        .then(savedPerson => {
+          response.json(savedPerson)
+        })
+        .catch(error => next(error))
+    })
+})
 
 
-  app.get('/api/persons', (request, response) => {
-    Person.find({})
-      .then(persons => {
-        response.json(persons)
-      })
-  })
+app.get('/api/persons', (request, response) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
