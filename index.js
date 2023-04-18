ac4d91dc8b1e236f0752b671fc228623c1756551
lniemi <90403204+lniemi@users.noreply.github.com>
2023-04-19 02:20:38 +0300

    Lint configuration (3.22)

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ app.use(cors())
 app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-morgan.token('body', (req, res) => {
+morgan.token('body', (req) => {
   return JSON.stringify(req.body)
 })
 
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-      response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
-    })
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
   })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
@@ -54,7 +54,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -63,17 +63,17 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-      const person = new Person({
-        name: body.name,
-        number: body.number
-      })
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-      person.save()
-        .then(savedPerson => {
-          response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
+    .catch(error => next(error))
+})
 
 
 
@@ -82,29 +82,29 @@ app.get('/api/persons', (request, response) => {
     .then(persons => {
       response.json(persons)
     })
-    .catch(error => next(error))
+    .catch(error => error)
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send( { error: 'unknown endpoint' } )
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
